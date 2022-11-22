1b463a047204743a702b43a3cfa6df6f6176ea54
Ganesh Darode <ganeshdarode121@gmail.com>
2022-11-22 15:19:41 +0530

    email verification fail added,replace get snapshot

--- src/Components/SignIn.js
+++ src/Components/SignIn.js
@@ -8,39 +8,33 @@ import { useAuthState } from 'react-firebase-hooks/auth'
 
 
 function SignIn() {
-    function signInWithGoogle() {
-        const provider = new firebase.auth.GoogleAuthProvider()
-        auth.signInWithPopup(provider);
-    }
     
     const [user] = useAuthState(auth)
-
+    // const [emailverification, setEmailverification] = useState("w")
+    var emailverification = "Verify the email before signing in!"
     async function signInWithEmail(){
         const email = document.getElementById('email').value;
         const password = document.getElementById('password').value;
         try {
-           await  auth.signInWithEmailAndPassword(email, password)
-           console.log('success');
-           console.log(user.emailVerified);
-           if(!user.emailVerified){
-            console.log('inside');
-                auth.signOut()
+           auth.signInWithEmailAndPassword(email, password).then(e=>{
+           if(!e.user.emailVerified){
+            console.log('email not verified');
+            auth.signOut();
            }
+           })
         } catch (error) {
             console.log(error);
         }
 
     }
     return (
-        // <div style={{ display: 'flex', justifyContent: 'center', height: '100vh', alignItems: 'center' }}>
-        //     <button style={{ padding: '30px', fontSize: '20px', borderRadius: '0', fontWeight: '600' }} onClick={signInWithGoogle}>Sign In With Google</button>
-        // </div>
-        <div>
+        <div style={{backgroundColor: "#FC8019",height:"100vh"}}>
             <form onSubmit={(e)=>e.preventDefault()}>
                 <h1>SignIn</h1>
         <input placeholder='email' id='email' required /><br></br>
         <input placeholder='password' id='password' required/><br></br>
         <button onClick={signInWithEmail}>SignIn</button><br></br>
+        <h2 style={{color:"red"}}>{emailverification}</h2>
         <NavLink to='/signup'><button >SignUp</button></NavLink>
         </form>
     </div>
